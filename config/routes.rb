73de8a260b63c23f28cb9e62fd92cc91936fb474
Miriam Grigsby <miriamgrigsby@gmail.com>
2020-06-11 17:49:08 -0600

    Patch: updates to postgres db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :spots
   resources :trials
   resources :positions
+  root  'controller_name#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
