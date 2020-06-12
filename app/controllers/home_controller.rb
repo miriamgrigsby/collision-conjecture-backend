028ff6d242aab66a3ae542ca3d81da57de3e568c
Miriam Grigsby <miriamgrigsby@gmail.com>
2020-06-11 18:36:11 -0600

    Patch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-    # render "index.html"
-    @spots = Spot.all
-    render json: @spots, root: false
+    render "index.html"
   end
 end
